7c1e3a4adc2487814ad7a8c7a942a1740afeba8c
EC2 Default User <ec2-user@ip-172-31-64-187.ec2.internal>
2022-04-02 19:45:11 +0000

    Section 3: The container module

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "IP_Address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "The IP Address and external port of the container"
-}
+# output "IP_Address" {
+#   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+#   description = "The IP Address and external port of the container"
+# }
 
-output "container_name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
+# output "container_name" {
+#   value       = docker_container.nodered_container[*].name
+#   description = "The name of the container"
+# }
