7966d118db2113a2705e7d3c79ff2d85534c529e
EC2 Default User <ec2-user@ip-172-31-64-187.ec2.internal>
2022-04-03 20:34:32 +0000

    Section 3: One Module for Multiple Resources

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-module "image" {
-  source = "./image"
-  image_in = var.image[terraform.workspace]
+module "nodered_image" {
+  source   = "./image"
+  image_in = var.image["nodered"][terraform.workspace]
+}
+
+module "influxdb_image" {
+  source   = "./image"
+  image_in = var.image["influxdb"][terraform.workspace]
 }
 
 resource "random_string" "random" {
@@ -11,13 +16,11 @@ resource "random_string" "random" {
 }
 
 module "container" {
-  source = "./container"
-  count = local.container_count
-  name_in  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in = module.image.image_out
-  int_port_in = var.int_port
-  ext_port_in = var.ext_port[terraform.workspace][count.index]
+  source            = "./container"
+  count             = local.container_count
+  name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
+  image_in          = module.nodered_image.image_out
+  int_port_in       = var.int_port
+  ext_port_in       = var.ext_port[terraform.workspace][count.index]
   container_path_in = "/data"
-  host_path_in      = "${path.cwd}/noderedvol"
 }
-
