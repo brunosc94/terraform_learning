34009adfb893781751af342105ab445b327a3487
EC2 Default User <ec2-user@ip-172-31-64-187.ec2.internal>
2022-04-03 18:06:15 +0000

    Section 3: Module Outputs

---
+++ container/outputs.tf
@@ -0,0 +1,10 @@
+#Output the IP address of the Container
+output "IP_address" {
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP Address and external port of the container"
+}
+
+output "container_name" {
+  value       = docker_container.nodered_container.name
+  description = "The name of the container"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
-# output "IP_Address" {
-#   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-#   description = "The IP Address and external port of the container"
-# }
+#Output the IP address of the Container
+output "IP_Address" {
+  value       = flatten(module.container[*].IP_address) #flatten achata as duas listas aqui presentes em uma.
+  description = "The IP Address and external port of the container"
+}
 
-# output "container_name" {
-#   value       = docker_container.nodered_container[*].name
-#   description = "The name of the container"
-# }
+output "container_name" {
+  value       = module.container[*].container_name
+  description = "The name of the container"
+}
