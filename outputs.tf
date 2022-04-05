a6014545d7f40d03be2849769a23c15c1482837d
Bruno Silva Cruz <bosc@gft.com>
2022-04-05 17:02:04 -0300

    Section 3: Deploying Containers with for_each

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-#Output the IP address of the Container
-output "ip-address" {
-  value       = flatten(module.container[*].IP_address) #flatten achata as duas listas aqui presentes em uma.
-  description = "The IP Address and external port of the container"
-}
+# #Output the IP address of the Container
+# output "ip-address" {
+#   value       = flatten(module.container[*].IP_address) #flatten achata as duas listas aqui presentes em uma.
+#   description = "The IP Address and external port of the container"
+# }
 
-output "container-name" {
-  value       = module.container[*].container_name
-  description = "The name of the container"
-}
+# output "container-name" {
+#   value       = module.container[*].container_name
+#   description = "The name of the container"
+# }
